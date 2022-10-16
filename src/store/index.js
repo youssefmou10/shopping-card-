2d82095fb06b428cf8aa32c7967bcdf799a6a913
youssefmou10 <mouddenyoussef98@gmail.com>
2022-10-16 13:08:07 +0100

    Sample

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,58 @@
 import { createStore } from 'vuex'
-
+import axios from "axios";
 export default createStore({
   state: {
+    products:[],
+    productsInBag:[]
   },
   mutations: {
+    loadProduct(state,products){
+        state.products=products
+    },
+    loadBag(state,products){
+      state.productsInBag=products
+     },
+    addToBag(state,product)
+    {
+      // ajouter des produits ou produit du panier
+      state.productsInBag.push(product);
+      localStorage.setItem("productsInBag",JSON.stringify(state.productsInBag))
+    },
+    removeFromBag(state,productId)
+    {
+      var updatedInBag=state.productsInBag.filter(item => productId !=item.id);
+      state.productsInBag=updatedInBag
+      localStorage.setItem("productsInBag",JSON.stringify(state.productsInBag))
+      }
   },
   actions: {
+      loadProduct({commit}){
+        axios.get('https://fakestoreapi.com/products').then(
+          response=>{
+            commit('loadProduct',response.data);
+          }
+        )
+      },
+      loadBag({commit}){
+        if(localStorage.getItem("productsInBag"))
+        {
+          commit('loadBag',JSON.parse(localStorage.getItem("productsInBag")));
+
+        }
+         
+        
+      },
+      addToBag({commit},product){
+        commit('addToBag',product)
+
+      },
+      removeFromBag({commit},productId){
+        if(confirm("Are you sure to remove this item ? "))
+        {
+          commit('removeFromBag',productId)
+        }
+       
+       }
   },
   modules: {
   }
